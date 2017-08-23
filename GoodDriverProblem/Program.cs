e6a34226ed2a5575de66a1ac1d87bea40c397502
Your Name <you@example.com>
2017-08-23 13:06:45 -0400

    break stream into different lists

--- GoodDriverProblem/Program.cs
+++ GoodDriverProblem/Program.cs
@@ -15,6 +15,10 @@ namespace GoodDriverProblem
 
             // Declare new List.
             List<string> names = new List<string>();
+            List<string> times = new List<string>();
+            List<string> miles = new List<string>();
+
+            string[] temp = new string[4];
 
             // Use using StreamReader for disposing.
             using (StreamReader r = new StreamReader(f))
@@ -26,21 +30,55 @@ namespace GoodDriverProblem
                     if (line.Contains("Trip"))
                     {
                         string[] info = line.Split(' ');
+
+
                         foreach (string i in info)
                         {
                             if (i != "Trip")
                             {
-                                names.Add(i);
+                                if (i.Contains(':'))
+                                {
+                                    
+                                }
+                                else if (i.Contains('.'))
+                                {
+                                    miles.Add(i);
+                                }
+                                else
+                                {
+                                    names.Add(i);
+                                }
                             }
                         }
                     }
                 }
             }
+            List<string> final = new List<string> { };
+            foreach (string i in names)
+            {
+                if (!final.Contains(i))
+                {
+                    final.Add(i);
+                }
+            }
 
-            foreach (string s in names)
+
+            foreach (string s in final)
             {
                 Console.WriteLine(s);
             }
+            //foreach (string s in names)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            //foreach (string s in miles)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            //foreach (string s in times)
+            //{
+            //    Console.WriteLine(s);
+            //}
         }
     }
 
